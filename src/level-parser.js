de3ac3c4c8c40937c232b354f356d3b7f0235acf
Marco361 <marek.giel@wp.pl>
2021-02-22 22:35:52 +0100

    Level-parser based on textRepresentation

--- src/level-parser.js
+++ src/level-parser.js
@@ -1,5 +1,44 @@
 import { Level } from './game/level';
+import { Vector2 } from './game/vector2';
+import { Tile } from './game/tile';
 
 export function parseLevel(textRepresentation) {
+  const tiles = [];
+  const lines = textRepresentation.split('\n');
+  for (let x = 0; x < lines.length; x++) {
+    const line = lines[x];
+    for (let y = 0; y < line.length; y++) {
+      const position = new Vector2(x + 1, y + 1);
+      const type = '';
+      switch (line[y]) {
+        case '.':
+          type = 'floor';
+          break;
+        case '#':
+          type = 'wall';
+          break;
+        case 'o':
+          type = 'storage';
+          break;
+        case 'x':
+          type = 'floor';
+          boxPositions = position;
+          break;
+        case 'X':
+          type = 'storage';
+          boxPositions = position;
+          break;
+        case 'p':
+          type = 'floor';
+          playerPosition = position;
+          break;
+        case 'P':
+          type = 'storage';
+          playerPosition = position;
+          break;
+      }
+      tiles.push(new Tile(type, position));
+    }
+  }
   return new Level(tiles, boxPositions, playerPosition);
 }
